9ddadeb69c2df35fca27e5890a8f6153104216ef
ginanjarn <ginanjarn@gmail.com>
2021-04-17 10:29:11 +0700

    hold menu check state

--- config.py
+++ config.py
@@ -12,6 +12,14 @@ F_DIAGNOSTIC = "diagnostic"
 F_VALIDATE = "validate"
 W_ABSOLUTE_IMPORT = "absolute_import"
 
+# menu check state holder
+AUTOCOMPLETE = False
+DOCUMENTATION = False
+DOCUMENT_FORMATTING = False
+DIAGNOSTIC = False
+VALIDATE = False
+ABSOLUTE_IMPORT = False
+
 
 @contextmanager
 def load_settings(save=False):
@@ -22,15 +30,38 @@ def load_settings(save=False):
         sublime.save_settings(SETTINGS_BASENAME)
 
 
+def update_menu():
+    """update menu check state"""
+
+    global AUTOCOMPLETE
+    global DOCUMENTATION
+    global DOCUMENT_FORMATTING
+    global DIAGNOSTIC
+    global VALIDATE
+    global ABSOLUTE_IMPORT
+
+    with load_settings(save=False) as settings:
+        AUTOCOMPLETE = settings.get(F_AUTOCOMPLETE, True)
+        DOCUMENTATION = settings.get(F_DOCUMENTATION, True)
+        DOCUMENT_FORMATTING = settings.get(F_DOCUMENT_FORMATTING, True)
+        DIAGNOSTIC = settings.get(F_DIAGNOSTIC, True)
+        VALIDATE = settings.get(F_VALIDATE, True)
+        ABSOLUTE_IMPORT = settings.get(W_ABSOLUTE_IMPORT, True)
+
+
+def plugin_loaded():
+    update_menu()
+
+
 class PytoolsSetAutocompleteCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         with load_settings(save=True) as settings:
             value = settings.get(F_AUTOCOMPLETE, True)
             settings.set(F_AUTOCOMPLETE, not value)
+            update_menu()
 
     def is_checked(self):
-        with load_settings() as settings:
-            return settings.get(F_AUTOCOMPLETE, True)
+        return AUTOCOMPLETE
 
 
 class PytoolsSetDocumentationCommand(sublime_plugin.TextCommand):
@@ -38,10 +69,10 @@ class PytoolsSetDocumentationCommand(sublime_plugin.TextCommand):
         with load_settings(save=True) as settings:
             value = settings.get(F_DOCUMENTATION, True)
             settings.set(F_DOCUMENTATION, not value)
+            update_menu()
 
     def is_checked(self):
-        with load_settings() as settings:
-            return settings.get(F_DOCUMENTATION, True)
+        return DOCUMENTATION
 
 
 class PytoolsSetDocumentformattingCommand(sublime_plugin.TextCommand):
@@ -49,10 +80,10 @@ class PytoolsSetDocumentformattingCommand(sublime_plugin.TextCommand):
         with load_settings(save=True) as settings:
             value = settings.get(F_DOCUMENT_FORMATTING, True)
             settings.set(F_DOCUMENT_FORMATTING, not value)
+            update_menu()
 
     def is_checked(self):
-        with load_settings() as settings:
-            return settings.get(F_DOCUMENT_FORMATTING, True)
+        return DOCUMENT_FORMATTING
 
 
 class PytoolsSetDiagnosticCommand(sublime_plugin.TextCommand):
@@ -60,10 +91,10 @@ class PytoolsSetDiagnosticCommand(sublime_plugin.TextCommand):
         with load_settings(save=True) as settings:
             value = settings.get(F_DIAGNOSTIC, True)
             settings.set(F_DIAGNOSTIC, not value)
+            update_menu()
 
     def is_checked(self):
-        with load_settings() as settings:
-            return settings.get(F_DIAGNOSTIC, True)
+        return DIAGNOSTIC
 
 
 class PytoolsSetValidateCommand(sublime_plugin.TextCommand):
@@ -71,10 +102,10 @@ class PytoolsSetValidateCommand(sublime_plugin.TextCommand):
         with load_settings(save=True) as settings:
             value = settings.get(F_VALIDATE, True)
             settings.set(F_VALIDATE, not value)
+            update_menu()
 
     def is_checked(self):
-        with load_settings() as settings:
-            return settings.get(F_VALIDATE, True)
+        return VALIDATE
 
 
 class PytoolsSetAbsoluteimportCommand(sublime_plugin.TextCommand):
@@ -82,7 +113,7 @@ class PytoolsSetAbsoluteimportCommand(sublime_plugin.TextCommand):
         with load_settings(save=True) as settings:
             value = settings.get(W_ABSOLUTE_IMPORT, True)
             settings.set(W_ABSOLUTE_IMPORT, not value)
+            update_menu()
 
     def is_checked(self):
-        with load_settings() as settings:
-            return settings.get(W_ABSOLUTE_IMPORT, True)
+        return ABSOLUTE_IMPORT
